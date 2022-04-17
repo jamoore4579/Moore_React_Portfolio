33ca59efffebbae456d37c60e27e688a55dae511
Irish Moore <jasonamoore@frontier.com>
2022-04-17 00:09:28 -0400

    build header with the nav bar

---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Navigation from "../Navigation";
+
+function Header(props) {
+    const { navigatePage, setNavigatePage} = props;
+
+    return (
+        <header>
+            <div>
+                <h2>Jason Moore</h2>
+            </div>
+            <div>
+                <Navigation
+                    navigatePage={navigatePage}
+                    setNavigatePage={setNavigatePage}
+                ></Navigation>
+            </div>
+        </header>
+    );
+}
+
+export default Headers;
