e6d8278544afa3d5aa2cae1974511a510311cf98
Irish Moore <jasonamoore@frontier.com>
2022-04-19 10:25:34 -0400

    coded helpers.js for validations

---
+++ src/utils/helpers.js
@@ -0,0 +1,13 @@
+export function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function validateEmail(email) {
+    var val = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return val.test(String(email).toLowerCase());
+}
+
+export function checkBlank(field) {
+    var val = /^\s*$/;
+    return val.test(String(field).toLowerCase());
+}
