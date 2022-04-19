491d24c96e1a932bfce67a28bf2c5d1a5cf10400
Irish Moore <jasonamoore@frontier.com>
2022-04-19 11:55:25 -0400

    coded footer file but still need images

---
+++ src/components/Footer/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div>
+                <a
+                    href="https://github.com/jamoore4579"
+                    target="_blank"
+                    rel="noopener noferrer"
+                >
+                    <img></img>
+                </a>
+                
+            </div>
+            <div>
+                <a
+                    href="https://github.com/jamoore4579"
+                    target="_blank"
+                    rel="noopener noferrer"
+                >
+                    <img></img>
+                </a>
+                
+            </div>
+            <div>
+                <a
+                    href="https://twitter.com/irish_moore5?t=iFZlCc5hc0_7hsdJbgKnHw&s=09"
+                    target="_blank"
+                    rel="noopener noferrer"
+                >
+                    <img></img>
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
