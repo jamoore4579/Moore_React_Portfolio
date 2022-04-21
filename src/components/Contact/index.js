beff97d700bc3fe09413c27fa0de0f677c07f58f
Irish Moore <jasonamoore@frontier.com>
2022-04-21 09:05:30 -0400

    changed the contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,87 +1,99 @@
-import React, { useState } from "react";
-import { validateEmail, validEmail } from "../../utils/helpers";
+import React from "react";
+import { MDBContainer, MDBRow, MDBCol } from "mdbreact";
+//import { validateEmail, validEmail } from "../../utils/helpers";
 
-function Contact() {
-    const [formState, setFormState] = useState({
-        name: "",
-        email: "",
-        message: "",
-    });
 
-    const [errorMessage, setErrorMessage] = useState("");
-
-    const { name, email, message } = formState;
-
-    function handleChange(e) {
-        if (e.target.name === "email") {
-            const isValid = validateEmail(e.target.value);
-            if (!isValid) {
-                setErrorMessage("Email is invalid");
-            } else {
-                if (!e.target.valule.length) {
-                    setErrorMessage(`${e.target.name} is required`);
-                } else {
-                    setErrorMessage("");
-                }
-            }
-        }
-
-        if (!errorMessage) {
-            setFormState({ ...formState, [e.target.name]: e.target.value })
-        }
-    }
-
-    function handleBlank(e) {
-        if (e.target.name === "Name" || e.target.name === "Message") {
-            if (!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required`);
-            } else {
-                setErrorMessage("");
-            }
-        }
-
-        if (!errorMessage) {
-            setFormState({ ...formState, [e.target.name]: e.target.value });
-        }
-    }
-
+const ContactInfo = () => {
     return (
-        <section>
-            <div className="center">
-                <h2 className="page-header">Contact Me</h2>
-            </div>
-            <div>
-                <form id="contact-form">
-                    <div>
-                        <label htmlFor="Name">NAme:</label>
-                        <br></br>
-                        <input
-                            type="text"
-                            defaultValue={name}
-                            onBlur={handleBlank}
-                            name="Name"
-                        />
+        <MDBContainer className="mt-5">
+            <MDBRow>
+                <MDBCol md="12" className="text-center">
+                <section>
+                    <div className="center">
+                        <h2 className="page-header">Contact Me</h2>
                     </div>
                     <div>
-                        <label htmlFor="Message">Message:</label>
-                        <br></br>
-                        <textarea
-                            name="Message"
-                            defaultValue={message}
-                            onBlur={handleBlank}
-                            rows="5"
-                        />
+                        <form id="contact-form">
+                            <div>
+                                <label htmlFor="Name">Name:</label>
+                                <br></br>
+                                <input
+                                    type="text"
+                                    defaultValue={name}
+                                    onBlur={handleBlank}
+                                    name="Name"
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="Message">Message:</label>
+                                <br></br>
+                                <textarea
+                                    name="Message"
+                                    defaultValue={message}
+                                    onBlur={handleBlank}
+                                    rows="5"
+                                />
+                            </div>
+                            {errorMessage && (
+                                <div>
+                                    <p className="error-text">{errorMessage}</p>
+                                </div>
+                            )}
+                            <button type="submit">Submit</button>
+                        </form>
                     </div>
-                    {errorMessage && (
-                        <div>
-                            <p className="error-text">{errorMessage}</p>
-                        </div>
-                    )}
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-        </section>
+                </section>
+                </MDBCol>
+            </MDBRow>
+        </MDBContainer>
     );
-}
+};
 
-export default Contact;
+export default ContactInfo;
+
+
+//import React, { useState } from "react";
+
+//function Contact() {
+//    const [formState, setFormState] = useState({
+//        name: "",
+//        email: "",
+//        message: "",
+//    });
+//
+//    const [errorMessage, setErrorMessage] = useState("");
+//
+//    const { name, email, message } = formState;
+//
+//    function handleChange(e) {
+//        if (e.target.name === "email") {
+//            const isValid = validateEmail(e.target.value);
+//            if (!isValid) {
+//                setErrorMessage("Email is invalid");
+//            } else {
+//                if (!e.target.valule.length) {
+//                    setErrorMessage(`${e.target.name} is required`);
+//                } else {
+//                    setErrorMessage("");
+//                }
+//            }
+//        }
+//
+//        if (!errorMessage) {
+//            setFormState({ ...formState, [e.target.name]: e.target.value })
+//        }
+//    }
+
+//    function handleBlank(e) {
+//        if (e.target.name === "Name" || e.target.name === "Message") {
+//            if (!e.target.value.length) {
+//                setErrorMessage(`${e.target.name} is required`);
+//            } else {
+//                setErrorMessage("");
+//            }
+//        }
+
+//        if (!errorMessage) {
+//            setFormState({ ...formState, [e.target.name]: e.target.value });
+//        }
+//    }
---
+++ src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Contact from "../components/Contact/index";
+import ContactInfo from "../components/Contact/index";
+import Wrapper from "../components/Wrapper/index";
+
+function Contact() {
+    return (
+        <Wrapper>
+            <ContactInfo />
+        </Wrapper>
+    )
+}
+
+export default Contact;
