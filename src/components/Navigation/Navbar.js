92cf97414aa5b92d5a5989a4f600b13dd66ed3a4
Irish Moore <jasonamoore@frontier.com>
2022-04-23 01:26:30 -0400

    Re-design navbar with styled components

--- src/components/Navigation/Navbar.js
+++ src/components/Navigation/Navbar.js
@@ -1,14 +1,67 @@
-import React from 'react'
-import { Nav } from './Navbar.elements'
+import React, {useState} from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { IconContext } from 'react-icons/lib'
+import { 
+  Nav, 
+  NavbarContainer, 
+  NavLogo, 
+  NavIcon, 
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks 
+} from './Navbar.elements'
 
-const index = () => {
+const Navbar = () => {
+  const [click, setClick] = useState(false)
+
+  const handleClick = () => setClick(!click)
+
   return (
     <div>
+      <IconContext.Provider value={{ color: '#fff' }}>
         <Nav>
-          
+          <NavbarContainer>
+            <NavLogo to="/">
+              <NavIcon />
+              JAM
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick}>
+              <NavItem>
+                <NavLinks to='/'>
+                  About
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavMenu onClick={handleClick}>
+              <NavItem>
+                <NavLinks to='/Portfolio'>
+                  Portfolio
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavMenu onClick={handleClick}>
+              <NavItem>
+                <NavLinks to='/Contact'>
+                  Contact
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+            <NavMenu onClick={handleClick}>
+              <NavItem>
+                <NavLinks to='/Resume'>
+                  Resume
+                </NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavbarContainer>
         </Nav>
+        </IconContext.Provider>
     </div>
   )
 }
 
-export default index
+export default Navbar
