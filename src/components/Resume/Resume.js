6bc4f42af97169e3d9631db95a9602a33adc5260
Irish Moore <jasonamoore@frontier.com>
2022-04-26 13:12:04 -0400

    updating resume page

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { Document, pdfjs } from "react-pdf"
 import { 
   ResumeWrapper,
 } from './Resume.elements';
-pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
 
 
 export const Resume = () => {
 
     return (
       <ResumeWrapper>
-        <Document
-          file="//github.com/jamoore4579/Moore_React_Portfolio/blob/main/src/components/assets/img/JasonMoore.pdf"
-        ></Document>
+        <a
+          
+        ></a>
       </ResumeWrapper>
     )
 };
