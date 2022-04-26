49b14d25764995b9bdefca0743660232fb353fb0
Irish Moore <jasonamoore@frontier.com>
2022-04-26 11:39:21 -0400

    added image to About me page

--- src/components/About/About.elements.js
+++ src/components/About/About.elements.js
@@ -1,46 +1,12 @@
 import styled from "styled-components";
 
-export const AboutSec = styled.div`
-color: #fff;
+export const AboutWrapper = styled.div`
 height: 500px;
-padding: 160px 0;
-background: ${({ lightBig}) => (lightBig ? '#fff' : '#101522')};
+background: #101522;
 `;
 
-export const AboutContainer = styled.div`
-
-`;
-
-export const AboutRow = styled.div`
-display: flex;
-margin: 0 -15px -15px -15px;
-flex-wrap: wrap;
-align-items: center;
-flex-direction: ${({ imgStart }) => (imgStart ? 'row-reverse' : 'row')};
-`;
-
-export const AboutColumn = styled.div`
-margin-bottom: 15px;
-padding-right: 15px;
-padding-left: 15px;
-flex: 1;
-max-width: 50%;
-flex-basis: 50%;
-
-@media screen and (max-width: 768px) {
-    max-width: 100%;
-    flex-basis: 100%;
-    display: flex;
-    justify-content: center;
-}
-`;
-
-export const TextWrapper = styled.div`
-max-width: 540px;
-padding-top: 0;
-padding-bottom: 30px;
-
-@media screen and (max-width: 768px) {
-    padding-bottom: 65px;
-}
+export const Image = styled.img`
+  width: 46%;
+  float: left;
+  margin-right: 10px;
 `;
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,26 +1,17 @@
 import React from "react";
 import { 
-    AboutSec,
-    AboutContainer, 
-    AboutRow, 
-    AboutColumn, 
-    TextWrapper 
+    AboutWrapper,
+    Image,
 } from './About.elements'
 
-//const profile = require('../../assets/img/self.jpg')
 
-export const About = () => (
-<div>
-    <AboutSec>
-        <AboutContainer>
-            <AboutRow>
-                <AboutColumn>
-                    <TextWrapper>
-                        Home Page
-                    </TextWrapper>
-                </AboutColumn>
-            </AboutRow>
-        </AboutContainer>
-    </AboutSec>
-</div>
-);
+export const About = () => {
+
+    const profile = require('../assets/img/self.jpg')
+
+    return (
+        <AboutWrapper>
+            <Image src={profile} />
+        </AboutWrapper>
+    )
+};
